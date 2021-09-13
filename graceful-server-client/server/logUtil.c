bdfe997891ef39e1f6fa0bfb14ec998e21f058ad
Hiroshi Sendai <hiroshi.sendai@kek.jp>
2021-09-13 12:57:20 +0900

    add "client:", "server:" in debug_print()

--- graceful-server-client/server/logUtil.c
+++ graceful-server-client/server/logUtil.c
@@ -71,6 +71,7 @@ int debug_print(FILE *pFILE,const char *fmt,...)
                 //	1900+pTm->tm_year,pTm->tm_mon+1,pTm->tm_mday,
                 //pTm->tm_hour,pTm->tm_min,pTm->tm_sec);
                 nRET += fprintf(pFILE, "%s.%06ld ", timestamp_buf, tv.tv_usec);
+                nRET += fprintf(pFILE, "server: ");
                 nRET += fprintf(pFILE,buff);
             }
         }
